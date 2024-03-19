2ad75e58a5ab9654b0f7ee0427309af7d732f1e1
Kittipat Choowongwan <130552287+bed7ime@users.noreply.github.com>
2024-03-19 12:31:59 +0700

    Delete print.js

--- print.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("place-order").addEventListener("click", function () {
-  // Capture the receipt container element
-  const receiptContainer = document.getElementById("cart");
-  // Use dom-to-image to convert the receipt container to an image
-  domtoimage.toBlob(receiptContainer).then(function (blob) {
-    // Save the image as a file using FileSaver.js
-    saveAs(blob, "shopping.png");
-  });
-});
